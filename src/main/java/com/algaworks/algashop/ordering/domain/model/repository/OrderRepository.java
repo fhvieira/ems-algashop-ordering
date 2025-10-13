1439c434c3a305c8b08d0268ea735be5145d62b8
fhvieira <fhvieira@hotmail.com>
2025-10-13 15:33:33 -0300

    refact order repository class name

--- src/main/java/com/algaworks/algashop/ordering/domain/model/repository/OrderRepository.java
+++ src/main/java/com/algaworks/algashop/ordering/domain/model/repository/OrderRepository.java
@@ -2,6 +2,7 @@ package com.algaworks.algashop.ordering.domain.model.repository;
 
 import com.algaworks.algashop.ordering.domain.model.entity.Order;
 import com.algaworks.algashop.ordering.domain.model.valueobject.id.OrderId;
+import org.springframework.stereotype.Component;
 
-public interface Orders extends Repository<Order,  OrderId>{
+public interface OrderRepository extends Repository<Order,  OrderId>{
 }
