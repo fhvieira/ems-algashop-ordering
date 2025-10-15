af12861d3cde7a36bd997062c835c61db7cd1320
fhvieira <fhvieira@hotmail.com>
2025-10-15 09:03:01 -0300

    add audit properties to order entity

--- src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/entity/OrderEntity.java
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/entity/OrderEntity.java
@@ -1,9 +1,14 @@
 package com.algaworks.algashop.ordering.infrastructure.persistence.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
@@ -18,6 +23,7 @@ import java.util.UUID;
 @ToString(of = "id")
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Table(name = "\"order\"")
+@EntityListeners(AuditingEntityListener.class)
 public class OrderEntity {
     @Id
     @EqualsAndHashCode.Include
@@ -31,4 +37,11 @@ public class OrderEntity {
     private OffsetDateTime paidAt;
     private OffsetDateTime canceledAt;
     private OffsetDateTime readyAt;
+
+    @CreatedBy
+    private UUID createdByUserId;
+    @LastModifiedDate
+    private OffsetDateTime lastModifiedAt;
+    @LastModifiedBy
+    private UUID lastModifiedByUserId;
 }
--- src/test/java/com/algaworks/algashop/ordering/infrastructure/persistence/repository/OrderEntityRepositoryIT.java
+++ src/test/java/com/algaworks/algashop/ordering/infrastructure/persistence/repository/OrderEntityRepositoryIT.java
@@ -1,16 +1,19 @@
 package com.algaworks.algashop.ordering.infrastructure.persistence.repository;
 
+import com.algaworks.algashop.ordering.infrastructure.persistence.config.SpringDataAuditingConfig;
 import com.algaworks.algashop.ordering.infrastructure.persistence.entity.OrderEntity;
 import com.algaworks.algashop.ordering.infrastructure.persistence.entity.OrderEntityTestDataBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import(SpringDataAuditingConfig.class)
 class OrderEntityRepositoryIT {
     private final OrderEntityRepository orderEntityRepository;
 
@@ -30,5 +33,15 @@ class OrderEntityRepositoryIT {
     @Test
     void shouldCount() {
         assertThat(orderEntityRepository.count()).isZero();
+    }
+
+    @Test
+    void shouldSetAuditingValues() {
+        OrderEntity entity = OrderEntityTestDataBuilder.existingBuilder().build();
+        entity = orderEntityRepository.saveAndFlush(entity);
+
+        assertThat(entity.getCreatedByUserId()).isNotNull();
+        assertThat(entity.getLastModifiedAt()).isNotNull();
+        assertThat(entity.getLastModifiedByUserId()).isNotNull();
     }
 }
